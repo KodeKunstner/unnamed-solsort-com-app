324673fde1997026345a623a57d769bcdf772dfa
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-01-30 13:57:09 +0100

    Commit unnamed-solsort-com-app.js via https://appedit.solsort.com/?Edit/js/gh/KodeKunstner/unnamed-solsort-com-app.

--- unnamed-solsort-com-app.js
+++ unnamed-solsort-com-app.js
@@ -1,29 +1,11 @@
-// # alfabet spil til Theodor
+// # solsort
 //
-// This is a bit of documentation, try 'Read' above. Code can be written as semi-literate code, see more here <https://en.wikipedia.org/wiki/Literate_programming>
 
 module.meta = {
-  id: 'alphabet-game',
+  id: 'solsort',
+  name: 'solsort utility library',
   version: '0.0.1',
-  npm: {}
+  npm: {
+  }
 };
 var da = require('direape@0.1');
-var jodom = require('direape@0.1/jodom');
-da.run(da.parent, 'reun:run', `
-console.log('here');
-var jsonml2dom = require('direape@0.1/jodom.js').jsonml2dom;
-console.log(jsonml2dom);
-
-`,'');
-
-da.run(da.parent, 'appedit:html',`
-<center>
-  <h1>Change me</h1>
-  <p>Try to edit the code.</p>
-  <p>Choose Edit above, and then<br>
-     alter the code on the left side...</p>
-  (vi keybindings is enabled,<br>
-  so press <tt>i</tt> to insert)
-</center>
-${Object.keys(require('lodash')).join('<br>')}
-`);
