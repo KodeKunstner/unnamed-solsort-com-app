75d392d42affd7664006757b6c11cd4780199bc1
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-01-30 13:55:05 +0100

    Commit unnamed-solsort-com-app.js via https://appedit.solsort.com/?Edit/js/gh/KodeKunstner/unnamed-solsort-com-app.

--- unnamed-solsort-com-app.js
+++ unnamed-solsort-com-app.js
@@ -1,12 +1,21 @@
-// # Hello world, this is a test
+// # alfabet spil til Theodor
 //
 // This is a bit of documentation, try 'Read' above. Code can be written as semi-literate code, see more here <https://en.wikipedia.org/wiki/Literate_programming>
 
 module.meta = {
-  name: 'Sample Application',
-  version: '0.0.1'
+  id: 'alphabet-game',
+  version: '0.0.1',
+  npm: {}
 };
 var da = require('direape@0.1');
+var jodom = require('direape@0.1/jodom');
+da.run(da.parent, 'reun:run', `
+console.log('here');
+var jsonml2dom = require('direape@0.1/jodom.js').jsonml2dom;
+console.log(jsonml2dom);
+
+`,'');
+
 da.run(da.parent, 'appedit:html',`
 <center>
   <h1>Change me</h1>
