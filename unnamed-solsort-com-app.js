0f6830a5ee2416ae74a22dab63fe00c26e43a442
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-02-01 12:26:15 +0100

    Commit unnamed-solsort-com-app.js via https://appedit.solsort.com/?Edit/js/gh/KodeKunstner/unnamed-solsort-com-app.

--- unnamed-solsort-com-app.js
+++ unnamed-solsort-com-app.js
@@ -1,11 +1,27 @@
-// # solsort
+// # alfabet spil til Theodor
 //
+// This is a bit of documentation, try 'Read' above. Code can be written as semi-literate code, see more here <https://en.wikipedia.org/wiki/Literate_programming>
 
 module.meta = {
-  id: 'solsort',
-  name: 'solsort utility library',
+  id: 'alphabet-game',
   version: '0.0.1',
-  npm: {
-  }
+  npm: {}
 };
-var da = require('direape@0.1');
+var ss = require('solsort@0.1');
+ss.html(`
+<style>
+#alphabet {
+display: inline-block;
+width: 100%;
+height: 100%;
+background: red;
+}
+</style>
+<div id=alphabet
+>
+${123}
+{ss.htmlEvent('hello')}
+"Hello world"
+</div>
+<center>
+`);
